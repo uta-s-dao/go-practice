282c2a71cf1571ba47e6a18a81e0bbc65b5db3b3
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-09-14 18:55:07 +0900

    function

--- 5/main.go
+++ 5/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"time"
 )
+
+func formatedDate(t time.Time) string {
+	layout := "2006-01-02 15:04:05"
+	return t.Format(layout)
+}
 
 func process(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("index.html", "index1.html")
 	t.Execute(w, "hello world")
+}
+
+func date(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{"fdate": formatedDate}
+	t := template.New("date.html").Funcs(funcMap)
+	t.ParseFiles("date.html")
+	t.Execute(w, time.Now())
 }
 
 func main() {
@@ -15,5 +28,6 @@ func main() {
 		Addr: "localhost:8080",
 	}
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/date", date)
 	server.ListenAndServe()
 }
