9e9d6940f872c71dd554f7d3e017b6367bf70486
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-09-24 22:35:12 +0900

    close method

--- 9/chanel.go
+++ 9/chanel.go
@@ -2,26 +2,61 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func thrower(c chan int) {
-	for i := 0; i < 5; i++ {
-		c <- i
-		fmt.Println("Threw >>", i)
-	}
+func callerA(c chan string) {
+	c <- "Hello World!"
+	close(c)
 }
 
-func catcher(c chan int) {
-	for i := 0; i < 5; i++ {
-		num := <-c
-		fmt.Println("Caught <<", num)
-	}
+func callerB(c chan string) {
+	c <- "Hola Mundo!"
+	close(c)
 }
 
 func main() {
-	c := make(chan int)
-	go thrower(c)
-	go catcher(c)
-	time.Sleep(100 * time.Millisecond)
+	a, b := make(chan string), make(chan string)
+	go callerA(a)
+	go callerB(b)
+	var msg string
+	ok1, ok2 := true, true
+
+	for ok1 || ok2 {
+		select {
+		case msg, ok1 = <-a:
+			if ok1 {
+				fmt.Printf("%s from a\n", msg)
+			}
+		case msg, ok2 = <-b:
+			if ok2 {
+				fmt.Printf("%s from b\n", msg)
+			}
+		}
+	}
 }
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func thrower(c chan int) {
+// 	for i := 0; i < 5; i++ {
+// 		c <- i
+// 		fmt.Println("Threw >>", i)
+// 	}
+// }
+
+// func catcher(c chan int) {
+// 	for i := 0; i < 5; i++ {
+// 		num := <-c
+// 		fmt.Println("Caught <<", num)
+// 	}
+// }
+
+// func main() {
+// 	c := make(chan int)
+// 	go thrower(c)
+// 	go catcher(c)
+// 	time.Sleep(100 * time.Millisecond)
+// }
