9863b1c49752b04da9d968b1516d33825a68074d
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-10-06 17:38:49 +0900

    #11 Update;display todo's struct

--- todo/main.go
+++ todo/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	// "encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
 )
+
+type todoList struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
 
 func todo(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("todo.html")
 	if err != nil {
 		fmt.Println("cannot parse todofiles")
 	}
-	t.Execute(w, "Hello world!")
+	list := []todoList{{Name: "guiter", Status: "complete"}, {Name: "dinner", Status: "todo"}}
+	// jsondata, err := json.MarshalIndent(list, "", "")
+	fmt.Println(list)
+	if err != nil {
+		fmt.Println("cannot marshal struct to json")
+	}
+	// fmt.Println(string(jsondata))
+	t.Execute(w, list)
 }
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/", todo)
-	fmt.Println("server on runnning:http://127.0.0.1:8080 ")
+	http.HandleFunc("/todo", todo)
+	fmt.Println("server on runnning:http://127.0.0.1:8080/todo")
 	server.ListenAndServe()
 }
